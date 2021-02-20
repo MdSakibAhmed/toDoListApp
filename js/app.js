f6d298824279ad1a98a325516eaa7748b79f95d7
Sakib Ahmed <sakibahmad99090@gmail.com>
2021-02-20 20:34:35 +0600

    some changes adde

--- js/app.js
+++ js/app.js
@@ -11,89 +11,94 @@ const displayInputBox = ()=>{
     element.addEventListener("click",function(){
         document.querySelector(".task-input").value ="";
         document.querySelector(".task-writing-box").style.display="block";
-
-       
         
     })
 }
 displayInputBox();
 
-const displayTask= (target)=>{
-    
 
+// Display task
+const displayTask= ()=>{
    
     document.getElementById("ok-btn").addEventListener("click",function(){
 
         const inputValue = document.querySelector(".task-input").value;
         const DateValue = document.querySelector(".task-date").value;
-        // if(target.outerHTML === `<i class="far fa-edit"></i>`){
-        //     target.parentNode.firstElementChild.innerHTML =`<i class="fas  fa-check-circle "></i>${target.parentNode.firstElementChild.innerText}` 
     
-    
-        // }
-        // else{
+       
             document.querySelector(".task-content-container").insertAdjacentHTML("beforeend",`<div class="task-content">
              
             <p> <i class="far fa-circle"></i>${inputValue}</p><i class="far fa-edit"></i><i class="fas fa-trash-alt"></i></div>`);
-          // inputValue = "";
-           document.querySelector(".task-writing-box").style.display="none";
-        //}
-
-       
+         
+           document.querySelector(".task-writing-box").style.display="none"; 
         
     })
 
 }
 displayTask();
 
-const updateTask = (e,inputBox)=>{
+
+
+const updateTask = (task,inputBox)=>{
     document.getElementById("edit-ok-btn").addEventListener("click",function(){
-        const inputValue = document.getElementById("edit-task-input").value;
+        const inputValue = document.getElementById("edit-task-input");
         console.log(inputValue);
-e = inputValue;
+       
+        task.innerHTML = `<i class="far fa-circle"></i>${inputValue.value}`;
+         console.log(task); 
 inputBox.style.display = "none";
 
     })
 }
+
+
+// compleat tas and edit and delete
 document.querySelector(".task-content-container").addEventListener("click",function(e){
-    console.dir(e.target);
+    //console.dir(e.target);
     if(e.target.classList[1] == "fa-edit"){
+        const inputValue = document.getElementById("edit-task-input");
        
         const DateValue = document.querySelector(".task-date").value;
         console.log(e.target);
         const inputBox = document.querySelector(".edit-task-writing-box");
         console.log(inputBox);
         inputBox.style.display="block";
+        inputValue.value = e.target.parentNode.firstElementChild.innerText;
+
+
         console.log(inputBox);
-        const text = e.target.parentNode.firstElementChild.innerText;
+        const text = e.target.parentNode.firstElementChild;
         console.log(text);
         updateTask(text,inputBox);
-       // inputBox.style.display = "none";
-
-        
-
 
     }
-    else if(e.target.outerHTML === `<i class="far fa-circle"></i>`){
+    else if(e.target.classList[1]=== `fa-circle` ){
       
             e.target.parentNode.classList.add("task-compleat");
-          
-                e.target.parentNode.innerHTML = `<i class="fas  fa-check-circle green-check-mark"></i>${e.target.parentNode.innerText}`;
- 
-    
+            console.dir(e.target.parentNode.parentNode);
+            e.target.parentNode.parentNode.childNodes[2].classList.remove("fa-edit");
+        
+                e.target.parentNode.innerHTML = `<i class="fas green-check-mark fa-check-circle"></i>${e.target.parentNode.innerText}`;
+        }
+        else if(e.target.classList[2] === `fa-check-circle`){
+            e.target.parentNode.classList.remove("task-compleat")
+            e.target.parentNode.parentNode.childNodes[2].classList.add("fa-edit");
+
+            e.target.parentNode.innerHTML = `<i class="far fa-circle "></i>${e.target.parentNode.innerText}`
+
         }
         else if(e.target.outerHTML === `<i class="fas fa-trash-alt"></i>` ){
             e.target.parentNode.innerHTML ="";
         }
-     
-   
-
-    // Edit task
-
- 
-   
-
-
 
 })
 
+// cancel button
+const buttons =document.getElementsByClassName("cancel");
+const newArray = Array.from(buttons);
+newArray.forEach(button =>{
+    button.addEventListener("click",function(e){
+        e.target.parentNode.parentNode.innerHTML ="";
+
+    })
+})
